e0a868f14092275082591eac7a6f0189dbe60bf4
бека турсункулов <tursunkulovbeka@gmail.com>
2022-08-31 18:21:41 +0600

    delete PL

--- Kabylan.PL/ViewModels/CustomerView.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Kabylan.PL.ViewModels {
-    public class CustomerView : PersonView {
-        public override string FirstName { get; set; } = null!;
-        public override string? MiddleName { get; set; } = null!;
-        public override string? LastName { get; set; } = null!;
-    }
-}
